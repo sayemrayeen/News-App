46df83af422dabf9b0fbd8540c08d4182d0e4943
Sayem <sayemrayeen068@gmail.com>
2022-07-04 10:40:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 
 export default class App extends Component {
   pageSize = 5;
+  apiKey = process.env.NEWS_API
 
   state ={
     progress : 0
---
+++ src/NewsItem.js
@@ -0,0 +1,48 @@
+import React  from "react";
+
+const NewsItem = (props)=> {
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className=" badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            src={
+              !imageUrl
+                ? "https://c.ndtvimg.com/2021-12/d8q0l77o_omicron-testing-afp-650_650x400_03_December_21.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default NewsItem;
